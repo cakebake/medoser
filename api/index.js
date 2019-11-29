d9e74f641ae74624f33c9e63be1a67dd0b5dea75
Jens A <ja@8works.de>
2019-11-29 10:23:16 +0100

    Fixes API status codes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { get, isEmpty } from 'lodash'
-import info from '../lib/youtube-dl/info'
+import { info } from '../lib/youtube-dl/info'
 // import validator from 'validator'
 // import xssFilters from 'xss-filters'
 
@@ -9,44 +9,58 @@ export const app = express()
 app.use(express.json())
 
 app.use((request, response, next) => {
-  response.removeHeader('X-Powered-By')
+  response
+    .removeHeader('X-Powered-By')
   next()
 })
 
 app.get('/', (request, response) => {
-  response.json({
-    success: true,
-    message: 'online'
-  })
+  response
+    .status(200)
+    .json({
+      success: true,
+      message: 'online'
+    })
 })
 
 app.post('/info', async (request, response) => {
+  let status = 200
   try {
     const url = get(request, 'body.url', null)
     if (isEmpty(url)) {
+      status = 400
       throw new Error('Parameter "url" is required')
     }
     const data = await info(url)
-    response.json({
-      success: true,
-      message: `Information for ${url} has been determined`,
-      data
-    })
+    response
+      .status(status)
+      .json({
+        success: true,
+        message: `Information for ${url} has been determined`,
+        data
+      })
   } catch (error) {
-    response.json({
-      success: false,
-      message: get(error, 'stderr', error.message),
-      data: null
-    })
+    if (status !== 400) {
+      status = 500
+    }
+    response
+      .status(status)
+      .json({
+        success: false,
+        message: get(error, 'stderr', error.message),
+        data: null
+      })
   }
 })
 
 app.post('/download', (request, response) => {
-  response.json({
-    success: true,
-    message: 'success',
-    data: request.body
-  })
+  response
+    .status(200)
+    .json({
+      success: true,
+      message: 'success',
+      data: request.body
+    })
 })
 
 export default {
